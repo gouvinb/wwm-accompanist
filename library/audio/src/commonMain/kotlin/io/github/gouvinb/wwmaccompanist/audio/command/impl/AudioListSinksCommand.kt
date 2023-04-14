292ee99c4543fb8cc356ac3b5acf61c84df5da1c
gouvinb <brunogouvinhas@gmail.com>
2023-04-14 13:03:05 +0200

    feature - Main: add logger to audio feature

--- library/audio/src/commonMain/kotlin/io/github/gouvinb/wwmaccompanist/audio/command/impl/AudioListSinksCommand.kt
+++ library/audio/src/commonMain/kotlin/io/github/gouvinb/wwmaccompanist/audio/command/impl/AudioListSinksCommand.kt
@@ -2,17 +2,20 @@ package io.github.gouvinb.wwmaccompanist.audio.command.impl
 
 import com.github.ajalt.clikt.parameters.options.versionOption
 import io.github.gouvinb.wwmaccompanist.audio.command.AudioSubCommand
+import io.github.gouvinb.wwmaccompanist.logger.presentation.log
 
 class AudioListSinksCommand : AudioSubCommand(
     name = "sinks",
     help = "List sinks",
-    helpTags = mapOf("version" to "0.1.1"),
+    helpTags = mapOf("version" to "0.1.2"),
 ) {
     init {
         versionOption(helpTags["version"]!!)
     }
 
     override fun run() {
-        echo(engine.listSinks().ifEmpty { null }?.map { it.value }?.joinToString("\n") ?: "No sink found.")
+        log.println {
+            engine.listSinks().ifEmpty { null }?.map { it.value }?.joinToString("\n") ?: "No sink found."
+        }
     }
 }
