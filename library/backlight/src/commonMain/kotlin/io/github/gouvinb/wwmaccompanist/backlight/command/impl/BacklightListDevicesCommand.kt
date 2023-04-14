91bc605e7ba19ae9132f109101811ad29e558ed3
gouvinb <brunogouvinhas@gmail.com>
2023-04-14 13:03:23 +0200

    feature - Main: add logger to backlight feature

--- library/backlight/src/commonMain/kotlin/io/github/gouvinb/wwmaccompanist/backlight/command/impl/BacklightListDevicesCommand.kt
+++ library/backlight/src/commonMain/kotlin/io/github/gouvinb/wwmaccompanist/backlight/command/impl/BacklightListDevicesCommand.kt
@@ -2,23 +2,24 @@ package io.github.gouvinb.wwmaccompanist.backlight.command.impl
 
 import com.github.ajalt.clikt.parameters.options.versionOption
 import io.github.gouvinb.wwmaccompanist.backlight.command.BacklightSubCommand
+import io.github.gouvinb.wwmaccompanist.logger.presentation.log
 
 class BacklightListDevicesCommand : BacklightSubCommand(
     name = "list",
     help = "List devices",
-    helpTags = mapOf("version" to "0.1.1"),
+    helpTags = mapOf("version" to "0.1.2"),
 ) {
     init {
         versionOption(helpTags["version"]!!)
     }
 
     override fun run() {
-        echo(
+        log.println {
             engine.listDevices()
                 .ifEmpty { null }
                 ?.toList()
                 ?.joinToString("\n") { "${it.first.padEnd(24)}\t${it.second}" }
-                ?: "No sink found.",
-        )
+                ?: "No sink found."
+        }
     }
 }
